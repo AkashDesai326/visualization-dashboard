742de850ef0d26a05e6b682e3ea81cd48b225071
akash <akashdesai326@gmail.com>
2020-11-17 13:56:04 +0530

    graph and data selector

--- games_of_data/dashboard/admin.py
+++ games_of_data/dashboard/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from .models import Customer,File
+from .models import Customer
 # Register your models here.
 
 @admin.register(Customer)
 class DashboardAdmin(admin.ModelAdmin):
-    list_display =("user_id","username","password","first_name","last_name","email","forgot_pwd_timestamp")
-
-@admin.register(File)
-class DashboardAdmin(admin.ModelAdmin):
-    list_display =("user_id","file_name","azur_file_name","azur_file_share","azur_container",)
+    list_display =("user_id","username","password","first_name","last_name","email")
